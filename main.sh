d7f5b84cd4217fd215cdab6795008b86b0673764
Natnael Teklu <natnaelteklu200@gmail.com>
2024-05-04 13:23:31 +0300

    commit before start

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
 # csv data analysis tool
 echo "this is the tool that will help you visualize and analyze your csv files"
-read -p "we need an imput now:" read
+read -p "we need an input now:" file
+cat $file
